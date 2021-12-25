268259145efb090929c2da7b5c115a8ff3e5eae6
mephit <meph.cell@gmail.com>
2021-12-26 02:26:15 +0300

    Add check file existing, change scale of pic

--- pic.py
+++ pic.py
@@ -1,8 +1,14 @@
+import os.path
+PATH, _ = os.path.split(os.path.abspath(__file__))  #Get path to script
+
 def pic(picname):
     import cairosvg
+    #Check file existing
+    if not os.path.exists(f"{PATH}/data/{picname}.png"):
 
-    #Convert icon to png and write to file
-    cairosvg.svg2png(url=f"https://yastatic.net/weather/i/icons/funky/dark/{picname}.svg", write_to=f"./data/{picname}.png", scale=10)
+        #Convert icon to png and write to file
+        cairosvg.svg2png(url=f"https://yastatic.net/weather/i/icons/funky/dark/{picname}.svg", \
+            write_to=f"{PATH}/data/{picname}.png", parent_height=300, parent_width=300, scale=7)
 
 
 
